b5e7062a5cd9b6926c6f3d37bfe6adba6f01308d
mistasse <istassem@gmail.com>
2020-01-02 10:27:11 +0100

    Fix releasing script

--- release.sh
+++ release.sh
@@ -4,7 +4,8 @@
 RELEASE_NUM=`grep version setup.py | cut -d\" -f2`
 
 # Push to PyPi
-python setup.py sdist upload
+python setup.py sdist
+twine upload dist/*
 
 # Tag in Git and push to remote
 git tag $RELEASE_NUM -m "Tagging release $RELEASE_NUM"
