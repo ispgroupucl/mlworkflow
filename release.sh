deb67b30f53e5d8c501aefca45ea12e16c4a2852
mistasse <istassem@gmail.com>
2020-01-06 14:13:15 +0100

    Fix release.sh

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ RELEASE_NUM=`grep version setup.py | cut -d\" -f2`
 
 # Push to PyPi
 python setup.py sdist
-twine upload dist/*
+twine upload dist/mlworkflow-$RELEASE_NUM.tar.gz
 
 # Tag in Git and push to remote
 git tag $RELEASE_NUM -m "Tagging release $RELEASE_NUM"
